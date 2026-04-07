6b33ffb8826b04cf2ef4c81e33b2f3e95da08538
gonzalez-7 <20agonzalezp@gmail.com>
2026-04-06 23:19:07 -0700

    Created the Product Model

--- COMP003B.LectureActivity3/Models/Product.cs
+++ COMP003B.LectureActivity3/Models/Product.cs
@@ -1,6 +1,19 @@
-﻿namespace COMP003B.LectureActivity3.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.LectureActivity3.Models
 {
 	public class Product
 	{
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength (100)]
+		public string Name { get; set; }
+
+		[Range (0.01, 10000)]
+		public decimal Price { get; set; }
+
+		[Required]
+		public string Category { get; set; }
 	}
 }
